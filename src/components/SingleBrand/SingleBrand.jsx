794ba2621e81a4006f6cee861abc6033c96a9c4a
nirobutso140 <nirobutso140@gmail.com>
2023-10-21 18:38:52 +0600

    get single data and display data on card

--- src/components/SingleBrand/SingleBrand.jsx
+++ src/components/SingleBrand/SingleBrand.jsx
@@ -1,6 +1,6 @@
 
 
-import {  useLoaderData } from 'react-router-dom';
+import {  Link, useLoaderData } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 import './SingleBrand.css'
 
@@ -27,31 +27,33 @@ const SingleBrand = () => {
                     <img src="/image/4.jpg" alt="Burger" />
                 </div>
             </div>
-            
-            <div className='product'>
-              {
-                // product.map(item => <>
 
-                //     <div className="card  card-side bg-base-100 shadow-xl w-[500px]">
-                //         <figure><img src={item.photo} className='w-[200px] h-full' alt="Movie" /></figure>
-                //         <div className="card-body">
-                //             <p>Brand: {item.brand}</p>
-                //             <p>Name: {item.name}</p>
-                //             <p>Type: {item.type}</p>
-                //             <p>Price: {item.price}</p>
-                //             <p>Rating: {item.rating}</p>
-                //             <div className="card-actions justify-end">
-                //                 <Link to={`/details/${item.brand}`}><button className="btn btn-primary">Details</button></Link>
-                //                 <button className="btn btn-primary">Update</button>
-                //             </div>
-                //         </div>
-                //     </div>
-                // </>)
 
-                <p>{product.brand}</p>
+           <div className='product'>
+           {
+                product.map(item => <>
+
+                    <div className="card  card-side bg-base-100 shadow-xl w-[500px]">
+                        <figure><img src={item.photo} className='w-[200px] h-full' alt="Movie" /></figure>
+                        <div className="card-body">
+                            <p>Brand: {item.brand}</p>
+                            <p>Name: {item.name}</p>
+                            <p>Type: {item.type}</p>
+                            <p>Price: {item.price}</p>
+                            <p>Rating: {item.rating}</p>
+                            <div className="card-actions justify-end">
+                                <Link to={`/details/${item.brand}`}><button className="btn btn-primary">Details</button></Link>
+                                <button className="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                    </div>
+                </>)
+
+                // <p>{product.brand}</p>
               }
-              
-            </div>
+           </div>
+            
+         
      
 
         </>
@@ -59,3 +61,6 @@ const SingleBrand = () => {
 };
 
 export default SingleBrand;
+
+
+   
