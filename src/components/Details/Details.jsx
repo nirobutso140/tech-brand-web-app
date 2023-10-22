a1e7aa06313cbc2631e8d55526d9960183c7b181
nirobutso140 <nirobutso140@gmail.com>
2023-10-22 16:02:05 +0600

    create brand-details route and view details of single product

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,14 +1,34 @@
-import { useLoaderData } from "react-router-dom";
-
+import {  useLoaderData } from "react-router-dom";
+import './Details.css'
 
 const Details = () => {
-
+  
    const details = useLoaderData()
-   const {brand} = details
+  
 
     return (
-        <div>
-            <p>{brand}</p>
+        <div className="details">
+            {
+                details.map(details => <>
+                    <div>
+                    <div className="card  card-side bg-base-100 w-[500px]">
+                        <figure><img src={details.photo} className='w-[200px] h-full' alt="Movie" /></figure>
+                        <div className="card-body">
+                            <p>Brand: {details.brand}</p>
+                            <p>Name: {details.name}</p>
+                            <p>Type: {details.type}</p>
+                            <p>Price: {details.price}</p>
+                            <p>Rating: {details.rating}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                    </div>
+                         <p className="details_desc">{details.description}</p>
+                    </div>
+                    
+                </>)
+            }
         </div>
     );
 };
